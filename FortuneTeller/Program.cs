3607bce416c645c40efafc8edb18bb68150836c5
NBeorn <nmbeorn@gmail.com>
2017-09-29 12:06:37 -0400

    Went to lunch

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -10,6 +10,34 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Queen Baba's Fortunes. Please enter your first name.");
+            string firstName = Console.ReadLine().Trim();
+
+            if(firstName.ToLower() == "goku")
+            {
+                Console.WriteLine("Glad someone got the reference.");
+            }
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine().Trim();
+
+            if (firstName.ToLower() + lastName.ToLower() == "danielvivacqua")
+            {
+                Console.WriteLine("Grade like Jarryd please. :)");
+            }
+            else if (firstName.ToLower() + lastName.ToLower() == "jarrydhuntley")
+            {
+                Console.WriteLine("Grade kindly :)");
+            }
+
+            Console.WriteLine("Please enter your age. And don't ask for mine!");
+            int age = int.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine("Please enter your birth month as a number. (January = 1, February = 2, etc.)");
+            int birthMonth = int.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine("Please enter your favorite ROYGBIV color. Enter 'Help' for a list of options");
+            Console.ReadLine();
         }
     }
 }
